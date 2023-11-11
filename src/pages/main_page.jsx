e3c365e9f06d926d80fece198583e5c0363c5a9e
артем <hajnkof@gmail.com>
2023-11-11 08:51:02 +0200

    test (basic)

---
+++ src/pages/main_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "../components/head/header";
+
+function MainPage(props) {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
+}
+
+export default MainPage;
